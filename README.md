c950c1b83355ef5efa7d179a03e3545e324bb772
Simon Deutsch <simon@codeandcomments.at>
2024-05-03 17:00:22 +0200

    first version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# cnc-eslint
+# @code-and-comments/eslint-config
 Default eslint configuration for projects of Code &amp; Comments
+
+## setup
+```shell
+npm i -D @ngrx/eslint-plugin
+ng add @angular-eslint/schematics
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+module.exports = {
+    ignorePatterns: [
+      "projects/**/*"
+    ],
+    overrides: [
+      {
+        files: ["*.ts"],
+        extends: [
+          "eslint:recommended",
+          "plugin:@typescript-eslint/recommended",
+          "plugin:@angular-eslint/recommended",
+          "plugin:@angular-eslint/template/process-inline-templates",
+          "plugin:@ngrx/eslint-plugin/recommended"
+        ],
+        rules: {
+          "object-curly-spacing": ["error", "always"],
+          "object-curly-newline": ["error"],
+          "operator-linebreak": ["error", "after", { "overrides": { ":": "before", "?": "before" } }],
+          "@angular-eslint/component-class-suffix": [
+            "error", { "suffixes": [ "Page", "Component", "Modal"] }
+          ],
+          "@angular-eslint/component-selector": [
+            "error",
+            {
+              "prefix": "app, input, cp",
+              "style": "kebab-case"
+            }
+          ]
+        }
+      },
+      {
+        files: ["*.html"],
+        extends: [
+          "plugin:@angular-eslint/template/recommended",
+          "plugin:@angular-eslint/template/accessibility"
+        ],
+        rules: {
+          "@angular-eslint/template/eqeqeq": ["error", { "allowNullOrUndefined": true }]
+        }
+      }
+    ]
+}
+  
